2fe1ef140777ee44b8a51b42162c9ca7bfe4b303
Feez-E <142937020+Feez-E@users.noreply.github.com>
2025-08-22 20:03:37 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -1,6 +1,11 @@
 let div = document.getElementById("div");
+let button = document.getElementById("button")
 
 div.addEventListener("click", () => {
     alert("Hola! Soy el div")
 
+})
+
+button.addEventListener("click", (e) => {
+    e.stopPropagation();
 })
